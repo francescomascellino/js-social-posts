1a62d47aab5f8d32322771db57beaab21034b934
francescomascellino <franesco.mascellino@gmail.com>
2023-09-09 01:50:09 +0200

    code cleaning

--- main.js
+++ main.js
@@ -102,39 +102,6 @@ const postsContainer = document.querySelector("#container.posts-list");
 
 let idArray = [];
 
-// const cardMarkup = `
-// <div class="post">
-// <div class="post__header">
-//     <div class="post-meta">                    
-//         <div class="post-meta__icon">
-//             <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-//         </div>
-//         <div class="post-meta__data">
-//             <div class="post-meta__author">Phil Mangione</div>
-//             <div class="post-meta__time">4 mesi fa</div>
-//         </div>                    
-//     </div>
-// </div>
-// <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-// <div class="post__image">
-//     <img src="https://unsplash.it/600/300?image=171" alt="">
-// </div>
-// <div class="post__footer">
-//     <div class="likes js-likes">
-//         <div class="likes__cta">
-//             <a class="like-button  js-like-button" href="#" data-postid="1">
-//                 <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-//                 <span class="like-button__label">Mi Piace</span>
-//             </a>
-//         </div>
-//         <div class="likes__counter">
-//             Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-//         </div>
-//     </div> 
-// </div>            
-// </div>
-// `;
-
 /* Milestone 2
 Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed. */
 
@@ -213,49 +180,6 @@ function generatePostCards(posts) {
 
 //ESEGUE generatePostCards LA PRIMA VOLTA
 generatePostCards(posts);
-
-//CODICE ORIGINALE DELLA FUNZIONE GENERA CARD
-/* posts.forEach(card => {
-
-    //PRENDO UN'IMMAGINA CASUALE DA LOREM PICSUM DA USARE COME FALLBACK
-    const fallBackAvatar = "https://picsum.photos/200"
-
-    const cardMarkup = `
-<div id="post_${card.id}" class="post">
-<div class="post__header">
-    <div class="post-meta">                    
-        <div class="post-meta__icon">
-            <img class="profile-pic" src="${card.author.image == null ? fallBackAvatar : card.author.image}" alt="${card.author.name}">                    
-        </div>
-        <div class="post-meta__data">
-            <div class="post-meta__author">${card.author.name}</div>
-            <div class="post-meta__time">${card.created}</div>
-        </div>                    
-    </div>
-</div>
-<div class="post__text">${card.content}</div>
-<div class="post__image">
-    <img src="${card.media}" alt="">
-</div>
-<div class="post__footer">
-    <div class="likes js-likes">
-        <div class="likes__cta">
-            <a class="like-button js-like-button" href="#" data-postid="${card.id}">
-                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                <span class="like-button__label">Mi Piace</span>
-            </a>
-        </div>
-        <div class="likes__counter">
-            Piace a <b id="like-counter-1" class="js-likes-counter">${card.likes}</b> persone
-        </div>
-    </div> 
-</div>            
-</div>
-`;
-
-    postsContainer.insertAdjacentHTML("beforeend", cardMarkup);
-
-}); */
 
 /* Milestone 3
 Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like. */
